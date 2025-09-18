1652993c4d40f08f9c04ddeaf92c673aefd9c78b
0ritam <mujritam085@gmail.com>
2025-09-18 22:35:32 +0530

    editing for build

--- src/app/(main)/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default async function MainPage() {
-  // Redirect to dashboard since this is now handled at the root
-  redirect("/dashboard");
-}
